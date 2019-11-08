06de662ad4bcb7995b78a7efd85e433402d1adcc
Imran Pochi <imran@kinvolk.io>
2019-11-08 15:22:35 +0530

    working changes with flannel cni
    Signed-off-by: Imran Pochi <imran@kinvolk.io>

--- assets.tf
+++ assets.tf
@@ -79,18 +79,18 @@ resource "template_dir" "kubernetes-chart" {
     aggregation_client_cert = "${var.enable_aggregation == "true" ? base64encode(join(" ", tls_locally_signed_cert.aggregation-client.*.cert_pem)) : ""}"
     aggregation_client_key  = "${var.enable_aggregation == "true" ? base64encode(join(" ", tls_private_key.aggregation-client.*.private_key_pem)) : ""}"
 
-    #  flannel_image           = "${var.container_images["flannel"]}"
-    #  flannel_cni_image       = "${var.container_images["flannel_cni"]}"
-    calico_image = "${var.container_images["calico"]}"
+    flannel_image     = "${var.container_images["flannel"]}"
+    flannel_cni_image = "${var.container_images["flannel_cni"]}"
 
-    calico_cni_image                = "${var.container_images["calico_cni"]}"
-    network_mtu                     = "${var.network_mtu}"
-    network_encapsulation           = "${indent(2, var.network_encapsulation == "vxlan" ? "vxlanMode" : "ipipMode")}"
-    ipip_enabled                    = "${var.network_encapsulation == "ipip" ? true : false}"
-    ipip_readiness                  = "${var.network_encapsulation == "ipip" ? indent(16, "- --bird-ready") : ""}"
-    vxlan_enabled                   = "${var.network_encapsulation == "vxlan" ? true : false}"
-    network_ip_autodetection_method = "${var.network_ip_autodetection_method}"
-    enable_reporting                = "${var.enable_reporting}"
+    # calico_image      = "${var.container_images["calico"]}"
+    # calico_cni_image                = "${var.container_images["calico_cni"]}"
+    # network_mtu                     = "${var.network_mtu}"
+    # network_encapsulation           = "${indent(2, var.network_encapsulation == "vxlan" ? "vxlanMode" : "ipipMode")}"
+    # ipip_enabled                    = "${var.network_encapsulation == "ipip" ? true : false}"
+    # ipip_readiness                  = "${var.network_encapsulation == "ipip" ? indent(16, "- --bird-ready") : ""}"
+    # vxlan_enabled                   = "${var.network_encapsulation == "vxlan" ? true : false}"
+    # network_ip_autodetection_method = "${var.network_ip_autodetection_method}"
+    # enable_reporting                = "${var.enable_reporting}"
   }
 }
 
