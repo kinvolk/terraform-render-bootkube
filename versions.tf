d61edff95296a4770317c459f24c0e2e80b7307d
Mauricio Vásquez <mauricio@kinvolk.io>
2019-12-16 17:06:52 -0500

    migrate to terraform 0.12
    - run terraform 0.12upgrade
    - use flatten for list
    - remove extra brackets in some places

--- versions.tf
+++ versions.tf
@@ -1,8 +1,7 @@
 # Terraform version and plugin versions
 
 terraform {
-  required_version = "~> 0.11.10"
-
+  required_version = ">= 0.12"
   required_providers {
     local    = "~> 1.2"
     template = "~> 2.1"
